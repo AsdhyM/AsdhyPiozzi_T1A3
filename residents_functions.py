9f9b21a4be74346cf50a377f6a0ee832ea13e214
Asdhy Piozzi <asdhyme@gmail.com>
2023-12-27 11:58:02 +1000

    Defined function for user to add new residents

--- residents_functions.py
+++ residents_functions.py
@@ -34,6 +34,28 @@ def create_connection(db_file):
 if __name__ == '__main__':
     create_connection(r"C:\sqlite\db\pythonsqlite.db")
 
+# Requesting input from user to add residents into dataframe
+def add_rows(residents_data):
+    more_residents = True
+    while more_residents:
+        new_resident = {'Room': (int(input("Enter Room number of new resident: "))),
+                        'Name': (int(input("Enter the fullname of new resident: "))),
+                        'Date_of_birth': (int(input("Enter Date of Birth of new resident: "))),
+                        'Sex': (int(input("Enter new resident's gender(female or male): "))),
+                        'Dementia': (int(input("Enter the type of Dementia of the new resident: "))),
+                        'Allergies': (int(input("Enter the new resident's allergies: "))),
+                        'Order': (int(input("Enter life/death order: ")))
+        }
+        residents_data = residents_data.append(new_resident, ignore_index = True)
+        more_residents = input('Add another [Y, N]: ')
+
+        if more_residents == 'N':
+            return residents_data
+            break
+        else:
+            continue
+df = add_rows(df)
+
 
 
 
